a869a743744abacf943d6f613feaf69a5fbcdb19
QEM <qem-19@mail.ru>
2017-03-16 12:55:16 +0300

    change dis (reverted from commit 59fe103e4d65fb87518bc4e7645f42c0d3bb51e0)

--- output_achiev.php
+++ output_achiev.php
@@ -5,11 +5,19 @@ for ($i = 0; $i < $result->num_rows; $i++) {
     $result->data_seek($i);
     $row = $result->fetch_row();?>
 
+<<<<<<< 0a9d4bed2da9487766836868182c672e965ea30c
+=======
+    <div>
+>>>>>>> 6b8ae8f0714adda652240e31bd12ac6c7b578d18
 <hr>
     Название: <?php echo getStringResult("SELECT name FROM `achievements` WHERE `id`='".$row[1]."'")?> <br>
     Фото: <img src="img/<?php echo $row[2];?>" width="100" height="100"><br>
     Описание: <?php echo $row[3];?>
 <hr>
+<<<<<<< 0a9d4bed2da9487766836868182c672e965ea30c
+=======
+    </div>
+>>>>>>> 6b8ae8f0714adda652240e31bd12ac6c7b578d18
 <?php
 }
 ?>
