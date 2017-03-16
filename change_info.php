eafbeed0db732748d3f1bc6660229ce335234f46
Nikkys96 <nik.sams@mail.ru>
2017-03-16 12:04:59 +0300

    Change_info

---
+++ change_info.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="main.css" rel="stylesheet">
+    <title>Редактирование информации</title>
+</head>
+<body>
+<?php
+include "functions.php";
+//проверяем авторизацию
+CheckAuth();
+//подключаем обработчик действий на этой странице
+include "change_handler.php";
+?>
+<a href="index.php"><h1>Главная страница</h1></a><br>
+<div class="left_block_add_achiev">
+    <?php
+    $result = ConnectBd()->query("SELECT * FROM `users` WHERE `id`='" . getIdByLogin($_COOKIE["login"]) . "'");
+    $result->data_seek();
+    $row = $result->fetch_row();
+    ?>
+</div>
+
+<div class="right_block_add_achiev">
+    <form action="" method="post" enctype="multipart/form-data">
+        Никнейм: <input type="text" size="30" name="nickname" value="<?php echo $row[0];?>"> <br><br>
+        Описание: <textarea cols="40" rows="5" name="description"><?php echo $row[1];?></textarea><br><br>
+
+        Фото: <input type="file" name="photo" id="idPhoto" accept="image/*"><br><br>
+        <input type="submit" value="Добавить">
+    </form>
+</div>
+</body>
+</html>
