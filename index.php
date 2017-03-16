b3603dcb581eb915e363af19f01d54ec1536612a
QEM <qem-19@mail.ru>
2017-03-16 14:31:35 +0300

    Change Design

--- index.php
+++ index.php
@@ -5,50 +5,21 @@ CheckAuth();
 
 <html>
 <head>
-    <title>Дневник Достиженийa</title>
+    <title>The Diary Achievements</title>
     <meta charset="UTF-8">
     <link href="main.css" rel="stylesheet">
 </head>
 
-<body class="main_style">
+<body class="body_style">
 
-<div class="head_logotip">
-    <p><font size="15" color="blace" >Дневник Достижений</font></p>
-    <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/core.css">
-</div>
+<div class="main_wrapper">
+    <div class="head_style"></div>
 
-<div>
-    <div class="photo">
-        <img src="imgs/photo.png" style="width:280px; height:300px; margin:40px 0 0 170px;" >
+    <div class="main_block">
+        <div class="login_main_block"><div class="login_text">QEM</div><img src="img/exit.png" width="30" height="30" class="exit_icon"></div>
+
     </div>
-
-
-    <div class="name">
-        <p><font size="5" text="center" color="black" >Глафира Родионовна Романова, 189 лет, <?php echo $_COOKIE["login"] ?> </font></p>
-
-
-        <div class="rating">
-            <p><font size="6" color="black" >1000%</font></p>
-        </div>
-    </div>
-
 </div>
-
-<html lang="en">
-<head>
-    <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-
-<a href="add_achiev.php" class="action-button shadow animate blue"> + Add</a>
-
-<a href="top_users.php" class="action-button shadow animate red">Top users</a>
-
-<a href="unaggree_achives.php" class="action-button shadow animate red">Agree</a>
-
-<?php include "output_achiev.php"?>
 
 </body>
 </html>
