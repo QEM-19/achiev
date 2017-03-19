f0c0da6bbcadc3b456306de3849a6dad62754620
QEM <qem-19@mail.ru>
2017-03-19 14:52:14 +0300

    change all design

--- index.php
+++ index.php
@@ -13,12 +13,41 @@ CheckAuth();
 <body class="body_style">
 
 <div class="main_wrapper">
-    <div class="head_style"></div>
-
-    <div class="main_block">
-        <div class="login_main_block"><div class="login_text">QEM</div><img src="img/exit.png" width="30" height="30" class="exit_icon"></div>
-
+    <div class="head_style">
+        <img src="img/logo.png" class="logo">
+        <img src="img/users.png" class="users">
     </div>
+
+
+<!--    блок информации и пользователе-->
+    <div class="main_block">
+        <div class="head_main_block">
+            <div class="login_main_block">QEM</div>
+            <img src="img/settings.png" class="settings">
+            <img src="img/exit.png" class="exit_icon">
+        </div>
+        <div class="ava" style="background: url(img/ava.jpg)"></div>
+        <div class="info">Я норм поц</div>
+        <div class="rate">100</div>
+        <div class="add">Добавить</div>
+        <div class="add">Unagree</div>
+    </div>
+<!--    ачивки пользователя-->
+
+    <div class="achiev">
+        <div class="ava_achiev" style="background: url(img/behemot.jpg)"></div>
+        <div class="head_achiev">
+            <div class="login_main_block">Сдох в альпах</div>
+            <img src="img/del.png" class="exit_icon">
+            <div class="status">-</div>
+        </div>
+        <div class="body_achiev">
+            <div class="info_achiev">Сдох крч</div>
+            <div class="date_achiev">10-10-2010</div>
+        </div>
+    </div>
+
+
 </div>
 
 </body>
