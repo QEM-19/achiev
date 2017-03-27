5016973114c72fe9458e058f091a3f6e5992fa3e
Nikkys96 <nik.sams@mail.ru>
2017-03-28 00:30:02 +0300

    ajax

--- change_info.php
+++ change_info.php
@@ -1,6 +1,6 @@
 <?php
-//подключаем обработчик действий на этой странице
-include "change_handler.php";
+
+include "functions.php";
 CheckAuth();
 ?>
 
@@ -9,6 +9,45 @@ CheckAuth();
     <title>The Diary Achievements</title>
     <meta charset="UTF-8">
     <link href="main.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script>
+        function funcSuccess(data){
+            alert("Соединение успешно");
+        }
+        function handler() {
+            var nick = document.getElementById("nick");
+            var desc = document.getElementById("desc");
+            if (nick.value.length >= 3 && nick.value.length <= 20){
+                var w_n = document.getElementById("warn_nick");
+                w_n.innerHTML = ' ';
+                if (desc.value.length >= 3 && desc.value.length <= 150){
+                    var w_n = document.getElementById("warn_desc");
+                    w_n.innerHTML = ' ';
+                    $.ajax({
+                        url: "change_handler.php",
+                        type: "POST",
+                        data: ({nickname:nick.value,description:desc.value}),
+                        dataType: "html",
+                        success: funcSuccess
+                    });
+                }
+                else{
+                    var w_n = document.getElementById("warn_desc");
+                    w_n.innerHTML = 'Неверная длина Описания'
+                }
+            }
+            else{
+                var w_n = document.getElementById("warn_nick");
+                w_n.innerHTML = 'Неверная длина Никнейма';
+                if (desc.value.length >= 3 && desc.value.length <= 150){
+                }
+                else{
+                    var w_n = document.getElementById("warn_desc");
+                    w_n.innerHTML = 'Неверная длина Описания'
+                }
+            }
+        }
+    </script>
 </head>
 
 <body class="body_style"><?php include "head.php"; ?>
@@ -23,12 +62,13 @@ CheckAuth();
 
     <div class="main_block">
         <div class="back_right_add">
-            <form action="change_handler.php" method="post" enctype="multipart/form-data">
-                Никнейм: <input type="text" size="30" name="nickname" value="<?php echo $row[1]; ?>"> <br><br>
-                Описание: <textarea cols="40" rows="5" name="description"><?php echo $row[2]; ?></textarea><br><br>
-                Фото: <input type="file" name="photo" id="idPhoto" accept="image/*"><br><br>
-                <input type="submit" value="Добавить">
-            </form>
+            <script>add_php()</script>
+            Никнейм: <input type="text" size="30" name="nickname" id="nick" value="<?php echo $row[1]; ?>"> <br>
+            <div id="warn_nick">    </div>
+            Описание: <textarea cols="40" rows="5" name="description" id="desc"><?php echo $row[2]; ?></textarea><br>
+            <div id="warn_desc"></div><br>
+            Фото: <input type="file" name="photo" id="idPhoto" accept="image/*"><br><br>
+            <input type="button" value="Изменить" onclick="handler()";>
         </div>
     </div>
 </div>
